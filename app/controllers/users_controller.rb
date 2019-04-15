ed616e5ce699b88d5ad37a66e9230ed09996393e
Caridad Rivera <ccrive10@gmail.com>
2019-04-15 15:02:14 -0400

    home page missing relationships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def home
+    @user = User.find_or_create_by(user_params)
+
+  end
+
+  def index
+    
+  end
+
+  def show
+
+  end
+
+  private
+
+  def user_params
+
+  end
 end
